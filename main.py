022804f6b6a280ff6d8e3d2e76df0398c8b23939
junde <jd.xu@siat.ac.cn>
2022-12-14 17:23:30 +0800

    push

--- main.py
+++ main.py
@@ -1,42 +1,79 @@
 import torch
+import math
+import sys
+import time
+import datetime
+import os
+import json
 
 from esm import FastaBatchedDataset, pretrained
 from data import MaskedBatchConverter
 from args import create_parser
-import math
-import sys
 from criterion import MaskedPredictionLoss
 from schedular import Scheduler, LinearScheduler
+import dist_misc
+from train import train_one_epoch
 
-### need transfer to distributed script ###
+
 def main(args):
+    # prepare model
     model, alphabet = pretrained.load_model_and_alphabet(args.model_location)
-    model.train()
     optimizer = torch.optim.AdamW(model.parameters(), betas=(0.9, 0.98), eps=10e-8, weight_decay=0.01)
     criterion = MaskedPredictionLoss()
     training_scheduler = Scheduler(optimizer, torch.cuda.amp.GradScaler(), LinearScheduler(args))
+    
+    return_contacts = "contacts" in args.include
+    assert all(-(model.num_layers + 1) <= i <= model.num_layers for i in args.repr_layers)
+    repr_layers = [(i + model.num_layers + 1) % (model.num_layers + 1) for i in args.repr_layers]
 
     if torch.cuda.is_available() and not args.nogpu:
         model = model.cuda()
         print("Transferred model to GPU")
 
+    # prepare dataset
     dataset = FastaBatchedDataset.from_file(args.fasta_file)
-    
+    ### change to dist sampler ###
     batches = dataset.get_batch_indices(args.toks_per_batch, extra_toks_per_seq=1)
-    
-    data_loader = torch.utils.data.DataLoader(
+    data_loader_train = torch.utils.data.DataLoader(
         dataset, collate_fn=MaskedBatchConverter(alphabet, args.truncation_seq_length), batch_sampler=batches
     )
-    
     print(f"Read {args.fasta_file} with {len(dataset)} sequences")
 
+    # prepare logger
     args.output_dir.mkdir(parents=True, exist_ok=True)
-    return_contacts = "contacts" in args.include
+    log_writer = None
 
-    assert all(-(model.num_layers + 1) <= i <= model.num_layers for i in args.repr_layers)
-    repr_layers = [(i + model.num_layers + 1) % (model.num_layers + 1) for i in args.repr_layers]
     
-    for 
+    dist_misc.load_model(args=args, model_without_ddp=model, scheduler=training_scheduler)
+
+    print(f"Start training for {args.epochs} epochs")
+    start_time = time.time()
+    for epoch in range(args.start_epoch, args.epochs):
+        if args.distributed:
+            data_loader_train.sampler.set_epoch(epoch)
+
+        train_stats = train_one_epoch(
+            model, data_loader_train,
+            criterion,  epoch, training_scheduler,
+            log_writer=None,
+            args=args
+        )
+        if args.output_dir and (epoch % 20 == 0 or epoch + 1 == args.epochs):
+            dist_misc.save_model(
+                args=args, model=model, model_without_ddp=model, scheuler=training_scheduler, epoch=epoch)
+
+        log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
+                        'epoch': epoch,}
+
+        if args.output_dir and dist_misc.is_main_process():
+            if log_writer is not None:
+                log_writer.flush()
+            with open(os.path.join(args.output_dir, "log.txt"), mode="a", encoding="utf-8") as f:
+                f.write(json.dumps(log_stats) + "\n")
+
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    print('Training time {}'.format(total_time_str))
 
 if __name__ == "__main__":
     parser = create_parser()
--- train.py
+++ train.py
@@ -1,10 +1,15 @@
-
 import torch
 import math
 import sys
 
-def train_one_epoch(model, data_loader, criterion, training_scheduler, epoch, args):
+import dist_misc
 
+def train_one_epoch(model, data_loader, criterion, training_scheduler, epoch, log_writer=None, args=None):
+    model.train()
+    metric_logger = dist_misc.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', dist_misc.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Epoch: [{}]'.format(epoch)
+    print_freq = 20
     accum_iter = 1 if args.accum_iter < 1 else args.accum_iter
 
     for batch_idx, (labels, strs, toks, masktoks, masks) in enumerate(data_loader):
@@ -32,9 +37,24 @@ def train_one_epoch(model, data_loader, criterion, training_scheduler, epoch, ar
 
         torch.cuda.synchronize()
 
-        representations = {
-            layer: t.to(device="cpu") for layer, t in out["representations"].items()
-        }
+        metric_logger.update(loss=loss_value)
 
+        lr = optimizer.param_groups[0]["lr"]
+        metric_logger.update(lr=lr)
+
+        loss_value_reduce = misc.all_reduce_mean(loss_value)
+        if log_writer is not None and (data_iter_step + 1) % accum_iter == 0:
+            """ We use epoch_1000x as the x-axis in tensorboard.
+            This calibrates different curves when batch size changes.
+            """
+            epoch_1000x = int((data_iter_step / len(data_loader) + epoch) * 1000)
+            log_writer.add_scalar('train_loss', loss_value_reduce, epoch_1000x)
+            log_writer.add_scalar('lr', lr, epoch_1000x)
+
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
 
 
