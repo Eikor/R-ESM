78f90c59793dff204710afccf4435f5f142f75eb
junde <jd.xu@siat.ac.cn>
2023-04-12 06:54:01 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
-OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=8 main.py \
-    /dros/common/working/jdxu/rnacentral_active_cleaned.fasta ncRNA_650M_coden3_epoch100/ \
+torchrun
+    --standalone
+    --nnodes=1
+    --nproc_per_node=8 main.py \
+    /dros/common/working/jdxu/rnacentral_active_cleaned.fasta 
+    ncRNA_650M_coden3_epoch100/ \
     --include contacts \
     --num_layers 33 \
     --embed_dim 1280 \
-    --coden_size 1
+    --coden_size 3
