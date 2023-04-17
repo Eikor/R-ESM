9a8a7528ada6f449f59274f70225cc848d4aab4f
junde <jd.xu@siat.ac.cn>
2023-04-17 15:25:00 +0000

    15B test

--- run.sh
+++ run.sh
@@ -42,19 +42,19 @@ torchrun \
     --nnodes=1 \
     --nproc_per_node=8 main.py \
     /dros/common/working/jdxu/snRNA.fasta \
-    snRNA_650M_coden3_epoch15/ \
+    snRNA_15B_coden3_epoch15/ \
     --include contacts \
     --save_interval 1 \
-    --num_layers 33 \
-    --num_heads 20 \
-    --embed_dim 1280 \
+    --num_layers 48 \
+    --num_heads 40 \
+    --embed_dim 5120 \
     --coden_size 3 \
     --warmup_epochs 1 \
     --epochs 15 \
     --lr 1e-3 \
     --min_lr 1e-5 \
     --accum_iter 384 \
-    --toks_per_batch 2052 \
+    --toks_per_batch 1024 \
     --truncation_seq_length 1024 \
     --fsdp \
     --bf16
